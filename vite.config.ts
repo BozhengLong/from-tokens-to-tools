21182ca4d23a2344345811231126ef31c9f71c21
Bozheng Long <im.bzlong@gmail.com>
2026-05-29 02:31:51 +0800

    chore: add @ path alias and YAML import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import yaml from '@rollup/plugin-yaml'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), yaml()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
